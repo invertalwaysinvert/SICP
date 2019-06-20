356ac376b3ed2b1dd5caf7431591825b3fc3ce48
Vinayak Kaushik <vinayakkaushik@protonmail.com>
2019-06-20 07:14:16 +0530

    Converts 1.4 from Scheme to Lisp

--- Chapter1/1.4.lisp
+++ Chapter1/1.4.lisp
@@ -1,6 +1,6 @@
 ;Exercise 1.4.  Observe that our model of evaluation allows for combinations whose operators are compound expressions. Use this observation to describe the behavior of the following procedure:
 
-(define (a-plus-abs-b a b)
+(defun (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
 ; This procedure adds the absolute value of b to a
