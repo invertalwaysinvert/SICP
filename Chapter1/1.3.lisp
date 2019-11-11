0a4d16a65bff638165f2b3548b3a45e4f4ffa086
Vinayak Kaushik <vinayakkaushik@protonmail.com>
2019-11-11 11:35:26 +0530

    Adds alternate implementation for 1.3 exercise

--- Chapter1/1.3.lisp
+++ Chapter1/1.3.lisp
@@ -7,3 +7,22 @@
   (cond ((and (<= a b) (<= a c)) (+ (square b) (square c)))
         ((and (<= b a) (<= b c)) (+ (square a) (square c)))
         (t (+ (square a) (square b)))))
+
+(defun sumOfLargest (a b c)
+  (if (>= a b)
+      (if (>= c b)
+          (+ (square a) (square c))
+          (+ (square a) (square b)))
+      (if (>= c a)
+          (+ (square b) (square c))
+          (+ (square a) (square b)))))
+
+(print (theBigThree 1 2 3))
+(print (sumOfLargest 1 2 3))
+(print (theBigThree 5 2 3))
+(print (sumOfLargest 5 2 3))
+(print (theBigThree 1 3 3))
+(print (sumOfLargest 1 3 3))
+(print (theBigThree 7 4 3))
+(print (sumOfLargest 7 4 3))
+
